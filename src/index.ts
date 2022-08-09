8cb49300ee86478e671f9ba1bd56d81b2261243e
fdolzanes1 <fdolzanes1@gmail.com>
2022-08-09 18:11:33 -0400

    enviroment variables

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,16 @@ import express from 'express';
 import routerProducts from './routes/products';
 import routerOrdered from './routes/ordered';
 import routerError from './routes/error';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({
+  path: path.resolve(__dirname, `${process.env.NODE_ENV}.env`)
+});
 
 const app = express();
-const PORT = 3000;
 
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(routerProducts);
@@ -16,6 +22,7 @@ app.get('/', (req, res) => {
   res.status(200).send({ "message": "Hello World" });
 });
 
-app.listen(PORT, ()=> {
-  console.log(`Listening on port ${PORT}`);
-});
+app.listen(process.env.PORT, () => {
+  console.log(`${process.env.NODE_ENV}.env`)
+  console.log(`APP LISTENING ON http://${process.env.HOST}:${process.env.PORT}`);
+})
