beb02086db142cfc2cc5b0d55959ef43edf7d9c3
fdolzanes1 <fdolzanes1@gmail.com>
2022-08-09 18:02:29 -0400

    CRUD com banco de dados

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,41 +1,71 @@
 import express from 'express';
+import pool from '../db/pg-connect';
+
 const router = express.Router();
-const path = '/products'
+const path = '/products';
+
 
 router.get(path, (req, res, next) => {
-  res.status(200).send({ 
-    message: "Using method GET in products"
+
+  pool.query('SELECT * FROM products', (error, data) => {
+    if (error) {
+      throw error
+    }
+    res.status(200).json(data.rows)
   });
+
 });
 
 router.get(path+'/:id_product', (req, res, next) => {
   const id = req.params.id_product;
-  res.status(200).send({ 
-    id: id,
-    message: "Using method GET in products"
+  
+  pool.query('SELECT * FROM products WHERE id_product = $1', [id], (error, data) => {
+    if (error) {
+      throw error
+    }
+    res.status(200).json(data.rows)
   });
+
 });
 
 router.post(path, (req, res, next) => {
-  res.status(201).send({ 
-    message: "Using method POST in products"
-  });
+
+  const { nome, preco } = req.body;
+
+  pool.query('INSERT INTO products (nome, preco) VALUES ($1, $2) RETURNING *', [nome, preco], (error, results) => {
+    if (error) {
+      throw error
+    }
+    res.status(201).send(`Products added with ID: ${results.rows[0].id_product}`)
+  })
 });
 
 router.put(path+'/:id_product', (req, res, next) => {
   const id = req.params.id_product;
-  res.status(200).send({ 
-    id: id,
-    message: "Using method PUT in products"
-  });
+  const { nome, preco } = req.body;
+
+  pool.query(
+    'UPDATE products SET nome = $1, preco = $2 WHERE id_product = $3',
+    [nome, preco, id],
+    (error, data) => {
+      if (error) {
+        res.status(400).send(`Error: ${error}`);
+        next()
+      }
+      res.status(200).send(`User modified with ID: ${id}`)
+    }
+  )
 });
 
 router.delete(path+'/:id_product', (req, res, next) => {
   const id = req.params.id_product;
-  res.status(201).send({ 
-    id: id,
-    message: "Using method DELETE in products"
-  });
+  pool.query('DELETE FROM products WHERE id_product = $1', [id], (error, data) => {
+    if (error) {
+      res.status(400).send(`Error: ${error}`);
+      next()
+    }
+    res.status(200).send(`Product deleted with ID: ${id}`)
+  })
 });
 
 export default router;
